16ee8f86f993727162927fecc3058e8f4d276eb3
Hua Huang <huangh223@gatech.edu>
2018-05-29 15:39:40 -0400

    Use cblas_dgemm for K matrix build

--- build_Fock.c
+++ build_Fock.c
@@ -10,6 +10,11 @@
 #include "TinySCF.h"
 #include "build_Fock.h"
 
+// If BLOCK_SIZE is too small, the flop-per-byte ratio is small for dgemm and cannot have good 
+// performance. The redundant calculation in K matrix build is BLOCK_SIZE / nbf * 100%.
+// Consider using a dynamic value later. 
+#define BLOCK_SIZE 32
+
 static int BLOCK_LOW(int i, int n, int size)
 {
 	int remainder = size % n;
@@ -21,27 +26,18 @@ static int BLOCK_LOW(int i, int n, int size)
 	return res;
 }
 
-void TinySCF_build_FockMat(TinySCF_t TinySCF)
+// Generate temporary array for J matrix and form J matrix
+// Low flop-per-byte ratio: access: nbf^2 * (df_nbf+1), compute: nbf^2 * df_nbf 
+static void build_J_mat(TinySCF_t TinySCF)
 {
-	double *Hcore_mat = TinySCF->Hcore_mat;
 	double *J_mat     = TinySCF->J_mat;
-	double *K_mat     = TinySCF->K_mat;
 	double *D_mat     = TinySCF->D_mat;
-	double *F_mat     = TinySCF->F_mat;
 	double *df_tensor = TinySCF->df_tensor;
 	double *temp_J    = TinySCF->temp_J;
-	double *temp_K    = TinySCF->temp_K;
 	int nbf           = TinySCF->nbasfuncs;
 	int df_nbf        = TinySCF->df_nbf;
 	int nthreads      = TinySCF->nthreads;
-
-	memset(temp_J, 0, DBL_SIZE * df_nbf);
-	memset(temp_K, 0, DBL_SIZE * nbf * nbf * df_nbf);
-
-	// UNDONE: (1) parallelize; (2) use dgemm for K matrix contraction
-
-	// Generate temporary array for J matrix and form J matrix
-	// Low flop-per-byte ratio: access: nbf^2 * (df_nbf+1), compute: nbf^2 * df_nbf 
+	
 	#pragma omp parallel
 	{
 		int tid  = omp_get_thread_num();
@@ -78,13 +74,24 @@ void TinySCF_build_FockMat(TinySCF_t TinySCF)
 				for (int p = 0; p < df_nbf; p++)
 					t += temp_J[p] * df_tensor_row[p];
 				J_mat[i * nbf + j] = t;
-				J_mat[j * nbf + i] = t;
 			}
 		}
 	}
+}
 
-	// Generate the temporary tensor for K matrix and form K matrix
-	// High flop-per-byte ratio: access: nbf^2 * (2*df_nbf+1), compute: nbf^3 * df_nbf
+// Generate the temporary tensor for K matrix and form K matrix
+// High flop-per-byte ratio: access: nbf^2 * (2*df_nbf+1), compute: nbf^3 * df_nbf
+// Note: the K_mat is not completed, the symmetrizing is done later
+void build_K_mat(TinySCF_t TinySCF)
+{
+	double *K_mat     = TinySCF->K_mat;
+	double *D_mat     = TinySCF->D_mat;
+	double *df_tensor = TinySCF->df_tensor;
+	double *temp_K    = TinySCF->temp_K;
+	int nbf           = TinySCF->nbasfuncs;
+	int df_nbf        = TinySCF->df_nbf;
+	
+	// Construct temporary tensor for K matrix
 	// Formula: temp_K(k, j, p) = dot(D_mat(k, 1:nbf), df_tensor(1:nbf, j, p))
 	for (int j = 0; j < nbf; j++)
 	{
@@ -95,27 +102,68 @@ void TinySCF_build_FockMat(TinySCF_t TinySCF)
 	}
 
 	// Build K matrix
-	for (int i = 0; i < nbf; i++)
+	// Formula: K(i, j) = sum_{k=1}^{nbf} [ dot(df_tensor(i, k, 1:df_nbf), temp_K(k, j, 1:df_nbf)) ]
+	memset(K_mat, 0, DBL_SIZE * nbf * nbf);
+	for (int i = 0; i < nbf; i += BLOCK_SIZE)
 	{
-		for (int j = i; j < nbf; j++)
+		int i_len = BLOCK_SIZE < (nbf - i) ? BLOCK_SIZE : (nbf - i);
+		for (int j = i; j < nbf; j += BLOCK_SIZE)
 		{
-			double t = 0;
-			
+			int j_len = BLOCK_SIZE < (nbf - j) ? BLOCK_SIZE : (nbf - j);
+			double *K_ij = K_mat + i * nbf + j;
 			for (int k = 0; k < nbf; k++)
 			{
-				double *temp_K_row = temp_K + (k * nbf + j) * df_nbf;
-				double *df_tensor_row = df_tensor + (i * nbf + k) * df_nbf;
-				#pragma simd
-				for (int p = 0; p < df_nbf; p++)
-					t += temp_K_row[p] * df_tensor_row[p];
+				double *df_tensor_i = df_tensor + (i * nbf + k) * df_nbf;
+				double *temp_K_j = temp_K + (k * nbf + j) * df_nbf;
+				cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, i_len, j_len, df_nbf,
+				            1.0, df_tensor_i, nbf * df_nbf, temp_K_j, df_nbf, 1.0, K_ij, nbf);
 			}
-
-			K_mat[i * nbf + j] = t;
-			K_mat[j * nbf + i] = t;
 		}
 	}
+}
 
-	// Build Fock matrix
-	for (int i = 0; i < nbf * nbf; i++)
-		F_mat[i] = Hcore_mat[i] + 2 * J_mat[i] - K_mat[i];
+void TinySCF_build_FockMat(TinySCF_t TinySCF)
+{
+	double *Hcore_mat = TinySCF->Hcore_mat;
+	double *J_mat = TinySCF->J_mat;
+	double *K_mat = TinySCF->K_mat;
+	double *F_mat = TinySCF->F_mat;
+	int nbf = TinySCF->nbasfuncs;
+	
+	double st0, st1, st2, et0, et1, et2;
+	
+	st0 = get_wtime_sec();
+	
+	// Build J matrix
+	st1 = get_wtime_sec();
+	build_J_mat(TinySCF);
+	et1 = get_wtime_sec();
+	
+	// Build K matrix
+	st2 = get_wtime_sec();
+	build_K_mat(TinySCF);
+	et2 = get_wtime_sec();
+	
+	// Symmetrizing J and K matrix and build Fock matrix
+	#pragma omp parallel
+	{
+		#pragma omp for schedule(dynamic)
+		for (int i = 1; i < nbf; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				J_mat[i * nbf + j] = J_mat[j * nbf + i];
+				K_mat[i * nbf + j] = K_mat[j * nbf + i];
+			}
+		}
+		
+		#pragma omp for 
+		for (int i = 0; i < nbf * nbf; i++)
+			F_mat[i] = Hcore_mat[i] + 2 * J_mat[i] - K_mat[i];
+	}
+	
+	et0 = get_wtime_sec();
+	printf("* Fock matrix build     : %.3lf (s)\n", et0 - st0);
+	printf("|--- J matrix build     |- %.3lf (s)\n", et1 - st1);
+	printf("|--- K matrix build     |- %.3lf (s)\n", et2 - st2);
 }
